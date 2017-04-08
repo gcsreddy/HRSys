358f164f59f271d2032f2affb2ff9ad7eb3f98bd
= <=iamnoant@gmail.com>
2017-04-08 14:57:51 -0500

    boostrap4 added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector:'dashboard',
-  templateUrl:'./dashboard.component.html'
+  templateUrl:'./dashboard.component.html',
+  styleUrls:['./dashboard.component.css']
 })
 export class DashboardComponent{
 
