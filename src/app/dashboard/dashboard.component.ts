3632ed5a0c5f54a27b5acd317fefd8791d5a14a2
GCS Reddy <iamnoant@gmail.com>
2017-04-08 23:48:09 -0500

    no errors after restructure

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { User } from '../user/user';
+import { UserService } from  '../user/user.service';
 @Component({
   selector:'dashboard',
   templateUrl:'./dashboard.component.html',
-  styleUrls:['./dashboard.component.css']
+  styleUrls:['./dashboard.component.css'],
+  providers:[UserService]
 })
 export class DashboardComponent{
 
